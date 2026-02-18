b6798e94eac39ba15d7418ecedb4fc8145ed4ebe
GregHdezQroMx <ghetzoom@gmail.com>
2026-02-18 07:41:45 -0600

    Project sync and build configs

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KommHotel"
+
+include(":composeApp")
+include(":server")
+include(":shared")
