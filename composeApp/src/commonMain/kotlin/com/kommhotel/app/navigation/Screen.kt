0261587b60d35dee7dc768d627c1b35bcfc58730
GregHdezQroMx <ghetzoom@gmail.com>
2026-02-21 22:32:28 -0600

    feat(booking): Create BookingScreen

--- composeApp/src/commonMain/kotlin/com/kommhotel/app/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/kommhotel/app/navigation/Screen.kt
@@ -7,10 +7,13 @@ import kotlinx.serialization.Serializable
  * A sealed class representing the screens in the main (authenticated) part of the app.
  * We use NavKey from navigation-3 for type-safe navigation.
  */
-sealed class Screen : NavKey {
+sealed class Screen : NavKey { 
     @Serializable
-    data object Home : Screen()
+    data object Home : Screen() // Shows the list of rooms
 
     @Serializable
     data class RoomDetail(val roomId: String) : Screen()
+
+    @Serializable
+    data object MyBookings : Screen() // New screen to show user's bookings
 }
